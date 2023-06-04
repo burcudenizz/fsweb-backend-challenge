f1c4e86654d7ce69a9b2efada089f7b45d6c8121
Burcu Deniz <dnzzburcu@gmail.com>
2023-06-04 13:09:15 +0300

    server çalıştırıldı.dosyalar oluşturuldu.

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const helmet = require("helmet");
+const server = express();
+
+server.use(helmet()); //  web uygulamalarını çeşitli güvenlik saldırılarına karşı korur.
+server.use(express.json());
+
+module.exports = server;
