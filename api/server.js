f0b9340e2176035c391663dd5cbdd493b237aaba
Burcu Deniz <dnzzburcu@gmail.com>
2023-06-06 10:29:44 +0300

    tweets mw güncelleme

--- api/server.js
+++ api/server.js
@@ -7,7 +7,12 @@ const server = express();
 
 server.use(helmet()); //  web uygulamalarını çeşitli güvenlik saldırılarına karşı korur.
 server.use(express.json());
-
+server.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); // Tüm kaynaklara erişime izin verir (Dikkat: Güvenlik riski oluşturabilir)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // İzin verilen HTTP yöntemleri
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // İzin verilen başlıklar
+    next();
+  });
 server.use("/api/auth", authRouter);
 server.use("/api/tweets", tweetsRouter);
 
