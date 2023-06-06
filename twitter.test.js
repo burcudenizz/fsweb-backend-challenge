81fe7215c1050c37d607a06ac1ed65130439f742
Burcu Deniz <dnzzburcu@gmail.com>
2023-06-06 19:14:30 +0300

    testler devam ediyor.

--- twitter.test.js
+++ twitter.test.js
@@ -5,7 +5,7 @@ const server = require("./api/server");
 afterAll(async () => {
   await db.destroy();
 });
-beforeEach(async () => {
+beforeAll(async () => {
   await db.migrate.rollback(); // yapılan değişiklikleri geri alır.
   await db.migrate.latest(); // yapılan değişiklikleri günceller.
   await db.seed.run(); //veritabanını doldurur.
@@ -68,7 +68,7 @@ describe("UserTestler", () => {
   test("[3] Post(/login) ile yanlış bilgiler girildiğinde hata mesajı dönüyor mu?", async () => {
     //arrange
     const userData = {
-      password: "123456",
+      password: "1234567",
       email: "gunsandroses@example.com",
     };
 
@@ -79,11 +79,44 @@ describe("UserTestler", () => {
     expect(actual.status).toBe(401);
   });
 
-  test("[4] Get(/users) ile kayıtlı kullanıcılar geliyor mu", async () => {
-    //act
-    let actual = await request(server).get("/api/auth/users");
+  test("[4]Get tüm kullanıcılar geliyor mu", async () => {
+    // Arrange
+    let loginPayload = {
+      password: "123456",
+      email: "gunsandroses@example.com",
+    };
+    let actual = await request(server)
+      .post("/api/auth/login")
+      .send(loginPayload);
+    expect(actual.status).toBe(200);
+    /// Act
+    const response = await request(server)
+      .get("/api/auth/users")
+      .set("authorization", actual.body.token);
 
-    //assert
-    expect(actual.body.length).toBe(3);
+    // Assert
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+});
+describe("Tweets testleri", () => {
+  test("[5] Get ile tüm tweetler geliyor mu?", async () => {
+    //act
+    let loginPayload = {
+      password: "123456",
+      email: "gunsandroses@example.com",
+    };
+    let actual = await request(server)
+      .post("/api/auth/login")
+      .send(loginPayload);
+    expect(actual.status).toBe(200);
+    /// Act
+    const response = await request(server)
+      .get("/api/tweets")
+      .set("authorization", actual.body.token);
+
+    // Assert
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
   });
 });
